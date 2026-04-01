bd4e7d116392af444e946cf607afbbd24dac8311
Sam Rosehill <samrosehill@icloud.com>
2026-04-01 13:41:06 +1100

    updates

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://samrosehill.com',
+  trailingSlash: 'never',
   integrations: [sitemap()],
   markdown: {
     shikiConfig: {
